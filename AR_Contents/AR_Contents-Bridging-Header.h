431b18a1d4e375d793e19eacc10187eb5be1fc68
Kotaro Sasagawa <b1900446@planet.kanazawa-it.ac.jp>
2022-12-18 00:26:52 +0900

    Initial Commit

---
+++ AR_Contents/AR_Contents-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  AR_Contents-Bridging-Header.h
+//  AR_Contents
+//
+//  Created by 山本知仁 on 2022/12/18.
+//
+
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "ShaderTypes.h"
